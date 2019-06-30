a695b7d2bb70c225f9700504e826bbc79a88014b
JsRicardo <502918488@qq.com>
2019-06-30 21:04:35 +0800

    server done

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,5 +13,6 @@ app.use(Express.json()) //使用中间件解析请求消息体中的JSON参数
 
 app.use("/api/movie", MovieRoute)
 app.use("/api/upload", UploadRoute)
+app.use("/upload", Express.static("public/upload"))
 
 app.listen(12306)
